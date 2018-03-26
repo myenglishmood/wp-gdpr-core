5162ec7618db28af7b1d1f6c405360cd2af26ce4
skurzynowski <sebastian.kurzynowski@gmail.com>
2018-03-26 14:46:44 +0200

    fix vesrion

--- wp-gdpr-core.php
+++ wp-gdpr-core.php
@@ -13,7 +13,7 @@
  * @wordpress-plugin
  * Plugin Name:       WP GDPR
  * Description:       Help to handle gdpr regulations.
- * Version:           1.4.2
+ * Version:           1.4.3
  * Text Domain:       wp_gdpr
  * Domain Path:       /languages
  * Author:            AppSaloon
