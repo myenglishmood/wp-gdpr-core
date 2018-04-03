2de8d37b243727074ebd81ecfd8167d00c42c9c0
skurzynowski <sebastian.kurzynowski@gmail.com>
2018-04-03 11:55:09 +0200

    add links

--- view/admin/menu/delete-page.php
+++ view/admin/menu/delete-page.php
@@ -6,15 +6,15 @@
 <div class="wrap">
     <h2><?php _e( 'List of delete requests', 'wp_gdpr' ); ?></h2>
     <div id="nav_menu">
-        <a id="a_delete" class="active_tab">Delete requests</a>
-        <a id="a_datarequest">Data Requests</a>
-        <a id="a_settings">Settings</a>
-        <a id="a_help">Help</a>
-        <a id="a_addon">Add-ons</a>
+        <a id="a_delete" href="<?php echo admin_url('admin.php?page=deletelist')?>" class="active_tab">Delete requests</a>
+        <a id="a_datarequest" href="<?php echo admin_url('admin.php?page=wp_gdpr')?>">Data Requests</a>
+        <a id="a_settings" href="<?php echo admin_url('admin.php?page=settings')?>">Settings</a>
+        <a id="a_help" href="<?php echo admin_url('admin.php?page=help')?>">Help</a>
+        <a target="_blank" id="a_addon" href="https://wp-gdpr.eu/add-ons/">Add-ons</a>
     </div>
     <div id="nav_menu_extra">
-        <a id="a_review">Review our plugin</a>
-        <a id="a_homepage">Visit our homepage</a>
+        <a id="a_review" target="_blank" href="https://wordpress.org/support/plugin/wp-gdpr-core/reviews/#new-post">Review our plugin</a>
+        <a id="a_homepage" target="_blank" href="https://wp-gdpr.eu/">Visit our homepage</a>
     </div>
     <div id="user_info">
         <div class="user_info_header">
