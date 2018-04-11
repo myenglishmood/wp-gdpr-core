1528f2b5f412c9ccaa917eae5baed3d1d9f62a20
Kim Vanrijckel <kim@MacBook-Pro.local>
2018-04-11 22:04:56 +0200

    almost ready for 1.5

--- assets/js/help.js
+++ assets/js/help.js
@@ -1,19 +1,33 @@
 jQuery(function ($) {
     //accordion
-    $( "#accordion" ).accordion({
+    $( "#accordion_core" ).accordion({
+        collapsible: true,
+        active: false,
+        heightStyle: "content",
+        autoHeight: false
+    });
+    $( "#accordion_addons" ).accordion({
         collapsible: true,
         active: false,
         heightStyle: "content",
         autoHeight: false
     });
     //tabs faq
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    });
 
     // Hide user info
     $('.user_info_header').click (function() {
         $('.user_info').hide()
     });
     //slick carousel
-
     $(".variable").slick({
         dots: false,
         slidesToShow: 2,
