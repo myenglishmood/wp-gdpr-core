e095c9acbc80cc344d4f735ea21ab958e7410217
Aytac Kokus <aytac@appsaloon.be>
2018-04-23 10:51:17 +0200

    Fix plugin activation hook and add deactivation hook to clear flag

--- config/activation-config.php
+++ config/activation-config.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace wp_gdpr\config;
 
 
@@ -6,14 +7,24 @@ use wp_gdpr\lib\Gdpr_Log;
 
 class Activation_Config {
 
+	public function __construct() {
+		$this->install();
+	}
+
 	/**
 	 * These functions are run when the plugin activates
 	 *
 	 * @since 1.5.3
 	 */
 	public function install() {
-		$this->create_logtable();
-		$this->delete_old_rows_in_logtable();
+		$did_the_script_already_run = get_option( 'gdpr_activation_script', true );
+
+		if ( $did_the_script_already_run !== '1' ) {
+			$this->create_logtable();
+			$this->delete_old_rows_in_logtable();
+
+			update_option( 'gdpr_activation_script', 1 );
+		}
 	}
 
 	/**
@@ -21,7 +32,7 @@ class Activation_Config {
 	 *
 	 * @since 1.5.3
 	 */
-	private function create_logtable(){
+	private function create_logtable() {
 		$log = Gdpr_Log::instance();
 
 		$log->create_log_table();
@@ -32,7 +43,7 @@ class Activation_Config {
 	 *
 	 * @since 1.5.3
 	 */
-	private function delete_old_rows_in_logtable(){
+	private function delete_old_rows_in_logtable() {
 		if ( ! wp_next_scheduled( 'gdpr_clear_log' ) ) {
 			wp_schedule_event( time(), 'daily', 'gdpr_clear_log' );
 		}
