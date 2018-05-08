dce470c65e4b9fe0a583914650c98f7a955c1e42
AlwaysQuads <kevin@192andup.com>
2018-05-08 15:47:21 +0200

    Manual scripts in template and delete unused file

--- view/front/gdpr-template.php
+++ view/front/gdpr-template.php
@@ -239,6 +239,47 @@ get_header();
 </div>
 <!-- end wrapper -->
 
+<script>var translatedText = {text1:"<?php echo var;?>",text2:"lkjoekljmldtext2"}</script>   
+    <script type="text/javascript">
+        $(document).ready(function() {
+
+
+            var table = $('#datatable-buttons').DataTable({
+                lengthChange: false,
+                buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+            });
+
+
+
+            $('#datatable-buttons').DataTable();
+
+
+
+            table.buttons().container()
+                .appendTo('#datatable-buttons_wrapper .col-md-6:eq(0)');
+        });
+    </script>
+    
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+
+
+            var table = $('#datatable-buttons2').DataTable({
+                lengthChange: false,
+                buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+            });
+
+
+
+            $('#datatable-buttons2').DataTable();
+
+
+            table.buttons().container()
+                .appendTo('#datatable-buttons2_wrapper .col-md-6:eq(0)');
+        });
+    </script>
+
 
 
 
