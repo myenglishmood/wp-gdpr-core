96729a461d554f3ec19a03e66c97d539654c962f
AlwaysQuads <kevin@192andup.com>
2018-05-25 16:12:42 +0200

    Styling and placement of delete button

--- view/admin/small-form-delete-request.php
+++ view/admin/small-form-delete-request.php
@@ -6,6 +6,8 @@
 ?>
 <form method="post" id="wgdpr_delete_comments_form">
     <input type="hidden"  name="gdpr_email" value="<?php echo $email; ?>">
-    <input type="submit" class="button button-primary" name="send_gdp_del_request" value="<?php _e('Send delete request', 'wp_gdpr'); ?>">
+    <button type="submit" class="btn btn-primary waves-effect waves-light" name="send_gdp_del_request" value="<?php _e('Send delete request', 'wp_gdpr'); ?>"><i class="fa fa-trash m-r-5"></i>
+    <?php _e('Send delete request', 'wp_gdpr'); ?>
+    </button>
     <input type="hidden" name="mail_action" value="gdpr">
 </form>
