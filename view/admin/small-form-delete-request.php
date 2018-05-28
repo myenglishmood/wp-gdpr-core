d1bb15adb5b1914d41f41801c9769ea5303a484f
AlwaysQuads <kevin@192andup.com>
2018-05-28 11:20:41 +0200

    Changes to the delete button

--- view/admin/small-form-delete-request.php
+++ view/admin/small-form-delete-request.php
@@ -4,9 +4,27 @@
  * to use in gdpr-template.php
  */
 ?>
+<script>
+$(".swa-confirm").on("click", function(e) {
+    e.preventDefault();
+    swal({
+        title: $(this).data("swa-title"),
+        text: $(this).data("swa-text"),
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#cc3f44",
+        confirmButtonText: $(this).data("swa-btn-txt"),
+        closeOnConfirm: true,
+        html: false
+    }, function( confirmed ) {
+        if( confirmed )
+            $(this).closest('form').submit();
+    });
+});
+</script>
 <form method="post" id="wgdpr_delete_comments_form">
     <input type="hidden"  name="gdpr_email" value="<?php echo $email; ?>">
-    <button type="submit" class="btn btn-primary waves-effect waves-light" name="send_gdp_del_request" value="<?php _e('Send delete request', 'wp_gdpr'); ?>"><i class="fa fa-trash m-r-5"></i>
+    <button id="sa-warning" class="swa-confirm btn btn-primary waves-effect waves-light" name="send_gdp_del_request" value="<?php _e('Send delete request', 'wp_gdpr'); ?>"><i class="fa fa-trash m-r-5"></i>
     <?php _e('Send delete request', 'wp_gdpr'); ?>
     </button>
     <input type="hidden" name="mail_action" value="gdpr">
